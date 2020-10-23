2a250cc5adeb46910f13237108b1ec7d2210edba
vhxubo <vhxubo@gmail.com>
2020-10-23 23:17:36 +0800

    Add day.js

--- index.js
+++ index.js
@@ -1,78 +1,84 @@
 require('dotenv').config();
 const got = require('got');
 const sha256 = require('crypto-js/sha256');
+const dayjs = require('dayjs');
 
 const API = {
-    login: 'http://hmgr.sec.lit.edu.cn/wms/healthyLogin',
-    report: 'http://hmgr.sec.lit.edu.cn/wms/addHealthyRecord',
-    lastRecord: 'http://hmgr.sec.lit.edu.cn/wms/lastHealthyRecord',
+  LOGIN: 'http://hmgr.sec.lit.edu.cn/wms/healthyLogin',
+  REPORT: 'http://hmgr.sec.lit.edu.cn/wms/addHealthyRecord',
+  LASTRECORD: 'http://hmgr.sec.lit.edu.cn/wms/lastHealthyRecord',
 };
 
 const headers = {
-    Host: 'hmgr.sec.lit.edu.cn',
-    Connection: 'keep-alive',
-    Accept: 'application/json, text/plain, */*',
-    DNT: '1',
-    'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64',
-    Referer: 'http://hmgr.sec.lit.edu.cn/web/',
-    'Accept-Encoding': 'gzip, deflate',
+  Host: 'hmgr.sec.lit.edu.cn',
+  Connection: 'keep-alive',
+  Accept: 'application/json, text/plain, */*',
+  DNT: '1',
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64',
+  Referer: 'http://hmgr.sec.lit.edu.cn/web/',
+  'Accept-Encoding': 'gzip, deflate',
 };
 
 const cardNo = process.env.CARDNO;
 const password = sha256(process.env.PASSWORD).toString();
 
 (async () => {
-    // 登录
-    const { body } = await got.post(API.login, {
-        json: {
-            cardNo: cardNo,
-            password: password,
-        },
-        headers,
-        responseType: 'json',
-    });
+  // 登录
+  const { body } = await got.post(API.LOGIN, {
+    headers,
+    json: {
+      cardNo: cardNo,
+      password: password,
+    },
+    responseType: 'json',
+  });
 
-    if (body.success) {
-        console.log('登录成功');
-    } else {
-        console.log('登录失败');
-    }
+  if (body.success) {
+    console.log('登录成功');
+  } else {
+    console.log('登录失败');
+  }
 
-    const token = body.data.token;
-    const teamId = body.data.teamId;
-    const userId = body.data.userId;
-    headers.token = token;
+  const token = body.data.token;
+  const teamId = body.data.teamId;
+  const userId = body.data.userId;
+  headers.token = token;
 
-    // 获取历史信息
-    const resp = await got(API.lastRecord, {
-        searchParams: { teamId, userId },
-        headers,
-        responseType: 'json',
-    });
+  // 获取历史信息
+  const resp = await got(API.LASTRECORD, {
+    headers,
+    searchParams: { teamId, userId },
+    responseType: 'json',
+  });
 
-    if (resp.body.success) {
-        console.log('获取历史数据成功');
-    } else {
-        console.log('获取历史数据失败');
-    }
+  if (resp.body.success) {
+    console.log('获取历史数据成功');
+    console.log(resp.body);
+  } else {
+    console.log('获取历史数据失败');
+    return;
+  }
 
-    const reportData = resp.body.data;
-    const temperature = (Math.random() * 0.6 + 36).toFixed(1).toString();
-    reportData.temperature = temperature;
-    reportData.isTrip = 0;
-    reportData.mobile = body.data.mobile;
+  const reportData = resp.body.data;
+  const temperature = (Math.random() * 0.6 + 36).toFixed(1).toString();
+  reportData.temperature = temperature;
+  reportData.temperatureTwo = temperature;
+  reportData.isTrip = 0;
+  reportData.mobile = body.data.mobile;
+  reportData.reportDate = dayjs().format('YYYY-MM-DD');
+  // 上报
+  console.log(reportData);
 
-    // 上报
-    //console.log(reportData);
-    const res = await got.post(API.report, {
-        json: reportData,
-        headers,
-        responseType: 'json',
-    });
-    console.log(res.body);
+  // Nothing....
+  // const res = await got.post(API.REPORT, {
+  //   json: reportData,
+  //   headers,
+  //   responseType: 'json',
+  // });
+  // console.log(res.body);
 
-    if (res.body.success) {
-        console.log('上报成功，今日体温：' + temperature);
-    }
+  // if (res.body.success) {
+  //   console.log('上报成功，今日体温：' + temperature);
+  // }
 })();
