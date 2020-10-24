79c6e56d4a1405f817af93b78f373e25bc31e6cd
vhxubo <vhxubo@gmail.com>
2020-10-24 11:39:06 +0800

    Del command

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const { program } = require('commander');
 const { Report } = require('./utils');
 
 program
-  .version('0.1.0')
-  .command('report')
+  .version('0.0.1')
   .requiredOption('-c, --cardNo <string>', 'cardNo is required')
   .requiredOption('-p, --password <string>', 'password is required')
   .requiredOption('-t,--temperature <float>', 'temperature is required');
