d426065bfc5d2bec37b79ec64b253af0fcc8b301
vhxubo <vhxubo@gmail.com>
2020-10-30 22:12:37 +0800

    Update to 1.1.0 code more beautiful

--- bin/lnr-cli.js
+++ bin/lnr-cli.js
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-const { Report } = require('../lib/report');
+const { program } = require('commander')
+const Report = require('../lib/report')
 
 program
-  .version('0.0.1')
+  .version('1.1.0')
   .command('report <cardNo>')
   .requiredOption('-p, --password <string>', 'password is required')
   .requiredOption('-t,--temperature <float>', 'temperature is required')
   .alias('r')
   .description('洛阳理工学院健康上报 CLI')
   .action((cardNo, cmdObj) => {
-    Report(cardNo, cmdObj.password, cmdObj.temperature).then((res) =>
-      console.log(res)
-    );
-  });
+    const { password, temperature } = cmdObj
 
-program.parse(process.argv);
+    console.table({ cardNo, temperature })
+    Report(cardNo, password, temperature).then((res) =>
+      console.log(`- ${res}`)
+    )
+  })
+
+program.parse(process.argv)
